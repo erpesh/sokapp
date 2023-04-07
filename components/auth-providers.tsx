2ae30a082ef9aa9a0162e84c2f7495ced3367d6c
erpesh <89254828+erpesh@users.noreply.github.com>
2023-04-07 20:12:09 +0100

    feat: Added isRegister to auth-providers.tsx

--- components/auth-providers.tsx
+++ components/auth-providers.tsx
@@ -3,10 +3,13 @@ import Image from "next/image";
 import googleIcon from "../assets/google-icon.png";
 import facebookIcon from "../assets/facebook-icon.svg";
 import {FacebookAuthProvider, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
-import {auth} from "../initFirebase";
+import {auth} from "../lib/initFirebase";
 
+interface Props {
+  isRegister?: boolean
+}
 
-const AuthProviders = () => {
+const AuthProviders = ({isRegister} : Props) => {
 
   const signInWithGoogle = () => {
     const provider = new GoogleAuthProvider();
@@ -28,11 +31,11 @@ const AuthProviders = () => {
     <div className={"auth-providers"}>
       <div onClick={signInWithGoogle} id="customBtn" className="customGPlusSignIn">
         <span className="icon"><Image alt={"Google icon"} src={googleIcon}/></span>
-        <span className="buttonText">Register with Google</span>
+        <span className="buttonText">{isRegister ? "Register" : "Log in"} with Google</span>
       </div>
       <div onClick={signInWithFacebook} id="customBtn" className="customGPlusSignIn">
         <span className="icon"><Image alt={"Google icon"} src={facebookIcon}/></span>
-        <span className="buttonText">Register with Facebook</span>
+        <span className="buttonText">{isRegister ? "Register" : "Log in"} with Facebook</span>
       </div>
     </div>
   );
